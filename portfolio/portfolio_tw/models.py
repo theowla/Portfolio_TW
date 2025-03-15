ca1ec0741dbecea42c4f772eb686636f22fdfb9e
theowla <theowlasi@gmail.com>
2025-03-15 21:21:36 +0100

    changes in models and some other files

--- portfolio/portfolio_tw/models.py
+++ portfolio/portfolio_tw/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    
+    title = models.CharField(max_length=100)
+    tech = models.CharField(max_length=100)
+    description = models.TextField()
+  
+    def __str__(self):
+        return f"{self.title} - {self.tech}"
+
+
+class Image(models.Model):
+    project = models.ForeignKey(Project, related_name="images", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="photos/")  
+
+    def __str__(self):
+        return f"Image for {self.project.title}"
