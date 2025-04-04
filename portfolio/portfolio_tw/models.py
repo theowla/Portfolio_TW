983b24ee1c072e0b561891ce77b6bf688b57fb38
Theo Wlasiczuk Ballesteros <124530613+theowla@users.noreply.github.com>
2025-04-04 12:16:18 +0000

    some code debugging and style changes

--- portfolio/portfolio_tw/models.py
+++ portfolio/portfolio_tw/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=100, default="Untitled Project")  # Default project name
+    description = models.TextField(blank=True, null=True)  # Optional field
+    image = models.ImageField(upload_to='images/')  # Field for the image
+
+    def __str__(self):
+        return self.description 
+    
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, related_name="images", on_delete=models.CASCADE)  # Link to the Post model
+    image = models.ImageField(upload_to='post_images/')  # Store images in /media/post_images/
+
+    def __str__(self):
+        return f"Image for Post {self.id}"  # Return a description like "Image for Post 1"
--- portfolio/portfolio_tw/views.py
+++ portfolio/portfolio_tw/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
-def home(request):
-    return render(request, "index.html")
+def home(request,):
+    posts = Post.objects.all
+    return render(request, "index.html", {'posts': posts})
+
+def post(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, "post.html", {'post': post}) #renders the page and gives you all the data of the Project you looked up
