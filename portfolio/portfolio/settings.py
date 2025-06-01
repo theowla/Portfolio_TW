d74a7d4b5c67f437bb59251d72f9b3001f4875c4
Theo Wlasiczuk Ballesteros <theoballesterosok@gmail.com>
2025-06-01 14:06:27 +0000

    settings

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # MEDIA
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
